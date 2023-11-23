2bf224582d8dfe3fd56a370fecfc00389acd79e8
Restusetiawan <restusetiawan161116@gmail.com>
2023-11-23 10:22:14 +0700

    repository baru

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Linux Administrator","Web Designer","Network Security","DevOps Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop:true
+})
